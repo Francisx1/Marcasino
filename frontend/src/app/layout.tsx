af17efab9cca669667a7f36021cbc78bb6c6dff0
Xunye <wang.xuny@northeastern.edu>
2026-02-11 16:20:20 +0800

    fixed a syntax error

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -14,12 +14,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
